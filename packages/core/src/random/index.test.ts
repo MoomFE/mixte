8dbddbdb249f73202ddd73591a21b8b83615ad0e
倚窗听雨丿醉无心 <994039348@qq.com>
2023-06-20 18:15:14 +0800

    新增 `randomNatural` 和 `random` 工具方法

---
+++ packages/core/src/random/index.test.ts
@@ -0,0 +1,96 @@
+import { random, randomNatural } from './index';
+
+describe('randomNatural', () => {
+  test('在传入的两个自然数之间随机生成一个自然数', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(randomNatural(0, 100));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 101 }, (_, i) => i),
+    );
+  });
+});
+
+describe('random', () => {
+  test('在传入的两个数字之间随机生成一个数字', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(random(0, 100));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 101 }, (_, i) => i),
+    );
+  });
+
+  test('支持传入数值均为负数', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(random(-100, -1));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 100 }, (_, i) => i - 100),
+    );
+  });
+
+  test('支持传入正数和负数', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(random(-50, 50));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 101 }, (_, i) => i - 50),
+    );
+  });
+
+  test('支持第一个参数大于第二个参数', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(random(50, -50));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 101 }, (_, i) => i - 50),
+    );
+  });
+
+  test('不传参数, 则默认在 0 和 10 之间随机生成一个数字', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 1000; i++)
+      nums.add(random());
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 11 }, (_, i) => i),
+    );
+  });
+
+  test('如果只传了一个参数, 则默认在 0 和传入参数之间随机生成一个数字', () => {
+    const nums = new Set<number>();
+
+    for (let i = 0; i < 10000; i++)
+      nums.add(random(100));
+
+    expect(
+      Array.from(nums).sort((a, b) => a - b),
+    ).toEqual(
+      Array.from({ length: 101 }, (_, i) => i),
+    );
+  });
+});
---
+++ packages/core/src/random/index.ts
@@ -0,0 +1,41 @@
+/**
+ * 在传入的两个自然数之间随机生成一个自然数
+ * @param from 最小的自然数
+ * @param to 最大的自然数
+ * @example
+ *
+ * randomNatural(0, 10); // -> 0 ~ 10
+ */
+export function randomNatural(from: number, to: number) {
+  return Math.floor(
+    Math.random() * (to - from + 1) + from,
+  );
+}
+
+/**
+ * 在传入的两个数字之间随机生成一个数字
+ * @example
+ *
+ * random(0, 10); // -> 0 ~ 10
+ * random(10, 0); // -> 0 ~ 10
+ * random(-10, 10); // -> -10 ~ 10
+ */
+export function random(...args: number[]) {
+  let [from = 0, to = 10] = args;
+
+  if (args.length === 1)
+    [from, to] = [0, from];
+
+  if (from > to)
+    [from, to] = [to, from];
+
+  if (from > 0)
+    return randomNatural(from, to);
+
+  const result = randomNatural(0, to + Math.abs(from));
+
+  if (result > to)
+    return to - result;
+
+  return result;
+}
