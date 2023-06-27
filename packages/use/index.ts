cdcbac470acb1f6c5dd82fd0c0295c22d8ee6b67
倚窗听雨丿醉无心 <994039348@qq.com>
2023-06-27 11:53:19 +0800

    完善打包相关代码

---
+++ packages/use/index.ts
@@ -0,0 +1,3 @@
+export * from './src/watch/index';
+export * from './src/whenever/index';
+export * from './src/wheneverEffectScope/index';
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,1 +1,62 @@
-console.log('Build');
+import { resolve } from 'node:path';
+import { dirname } from '@moomfe/small-utils/node-utils';
+import { build } from 'vite';
+import { rollup } from 'rollup';
+import dts from 'rollup-plugin-dts';
+
+/** 项目根目录 */
+const rootPath = resolve(dirname(import.meta), '../');
+/** 代码根目录 */
+const srcPath = resolve(rootPath, 'packages');
+
+// Core
+await build({
+  build: {
+    outDir: resolve(srcPath, 'core/dist'),
+    lib: {
+      entry: resolve(srcPath, 'core/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: format => `index.${format === 'es' ? 'mjs' : format}`,
+    },
+    minify: false,
+  },
+});
+await rollup({
+  input: resolve(srcPath, 'core/index.ts'),
+  plugins: [
+    dts({ respectExternal: true }),
+  ],
+}).then((bundle) => {
+  bundle.write({
+    file: resolve(srcPath, 'core/dist/index.d.ts'),
+    format: 'es',
+  });
+});
+
+// Use
+await build({
+  build: {
+    outDir: resolve(srcPath, 'use/dist'),
+    lib: {
+      entry: resolve(srcPath, 'use/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: format => `index.${format === 'es' ? 'mjs' : format}`,
+    },
+    minify: false,
+    rollupOptions: {
+      external: ['vue-demi', '@vueuse/core'],
+    },
+  },
+});
+await rollup({
+  input: resolve(srcPath, 'use/index.ts'),
+  plugins: [
+    dts({ respectExternal: true }),
+  ],
+  external: ['vue-demi', '@vueuse/core'],
+}).then((bundle) => {
+  bundle.write({
+    file: resolve(srcPath, 'use/dist/index.d.ts'),
+    format: 'es',
+  });
+});
