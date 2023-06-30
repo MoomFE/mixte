05be78d33da092328ff8472cc195dbb31d34d50d
倚窗听雨丿醉无心 <994039348@qq.com>
2023-06-30 09:17:17 +0800

    打包后拷贝 LICENSE 文件至子项目目录

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,18 @@
+import { resolve } from 'node:path';
 import { execSync } from 'node:child_process';
+import fs from 'fs-extra';
+import { packages } from '../meta/packages';
 
+const rootDir = resolve(__dirname, '../');
+const packagesDir = resolve(rootDir, 'packages');
+
+// 打包代码
 execSync('rollup -c', { stdio: 'inherit' });
+
+// 拷贝 LICENSE 文件至子项目目录
+packages.forEach(({ name }) => {
+  fs.copyFileSync(
+    resolve(rootDir, 'LICENSE'),
+    resolve(packagesDir, name, 'LICENSE'),
+  );
+});
