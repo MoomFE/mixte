9fe5ccffa11f0e778ef3a9732b22abecb6796131
倚窗听雨丿醉无心 <994039348@qq.com>
2023-07-03 15:25:05 +0800

    按需导入支持传入 `useWithVueUseCore` 选项排除与 `@vueuse/core` 功能相同且名称相同的方法

--- packages/use/test/vite.test.ts
+++ packages/use/test/vite.test.ts
@@ -9,13 +9,35 @@ describe('MixteUseAutoImport', () => {
     const result = MixteUseAutoImport();
 
     expect(result).toHaveProperty('@mixte/use');
-    expect(Object.keys(result).length).toBe(1);
+    expect(Object.keys(result)).toHaveLength(1);
   });
 
   test('默认情况下返回所有导出的方法', async () => {
-    const mixteUse = Object.keys(await import('@mixte/use')).sort();
     const autoImport = MixteUseAutoImport()['@mixte/use'].sort();
+    const mixteUse = Object.keys(await import('@mixte/use')).sort();
 
     expect(autoImport).toEqual(mixteUse);
+  });
+
+  test('支持传入 `useWithVueUseCore` 选项标识是和 `@vueuse/core` 一起使用的场景, 会排除与 `@vueuse/core` 功能相同且名称相同的方法', async () => {
+    const autoImport = MixteUseAutoImport({ useWithVueUseCore: true })['@mixte/use'].sort();
+    const mixteUse = Object.keys(await import('@mixte/use')).sort();
+    const vueuse = Object.keys(await import('@vueuse/core')).sort();
+
+    expect(autoImport).not.toEqual(mixteUse);
+
+    // 排除了 `@vueuse/core` 中已经导出的方法
+    autoImport.forEach((name) => {
+      expect(vueuse).not.toContain(name);
+    });
+
+    // 默认导出中含有与 `@vueuse/core` 名称相同的方法
+    let count = 0;
+
+    mixteUse.forEach((name) => {
+      if (vueuse.includes(name)) count++;
+    });
+
+    expect(count > 0).toBe(true);
   });
 });
--- packages/use/vite.ts
+++ packages/use/vite.ts
@@ -1,5 +1,16 @@
 import type { ImportsMap } from 'unplugin-auto-import/types';
 
+interface MixteUseAutoImportOptions {
+  /**
+   * 是否和 `@vueuse/core` 一起使用
+   *  - 会排除与 `@vueuse/core` 功能相同且名称相同的方法
+   *  - 例如 `watchDeep`, `watchImmediate`, `whenever` 等
+   *
+   * @default false
+   */
+  useWithVueUseCore?: boolean
+}
+
 /**
  * 按需导入
  *  - 供 [unplugin-auto-import](https://github.com/antfu/unplugin-auto-import) 使用
@@ -11,19 +22,36 @@ import type { ImportsMap } from 'unplugin-auto-import/types';
  *
  * export default defineConfig({
  *   plugins: [
+ *     // 导入所有方法
  *     AutoImport({
  *       imports: [MixteUseAutoImport()]
  *     }),
+ *     // 与 `@vueuse/core` 一起使用时
+ *    AutoImport({
+ *      imports: [
+ *        '@vueuse/core',
+ *        MixteUseAutoImport({ useWithVueUseCore: true }),
+ *      ],
  *   ],
  * })
  */
-export function MixteUseAutoImport(): ImportsMap {
+export function MixteUseAutoImport(options?: MixteUseAutoImportOptions): ImportsMap {
+  const {
+    useWithVueUseCore = false,
+  } = options ?? {};
+
   return {
     '@mixte/use': [
-      'watchDeep',
-      'watchImmediate',
+      ...(
+        useWithVueUseCore
+          ? []
+          : [
+              'watchDeep',
+              'watchImmediate',
+              'whenever',
+            ]
+      ),
       'watchImmediateDeep',
-      'whenever',
       'wheneverDeep',
       'wheneverEffectScope',
       'wheneverEffectScopeImmediate',
