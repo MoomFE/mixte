b5b24b880f2dea19780f2e68fc88fb4bd292d9e0
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-02 19:50:15 +0800

    [docs] 细节样式微调

--- packages/.vitepress/components/DemoCard.vue
+++ packages/.vitepress/components/DemoCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-lg bg-neutral-1 dark:bg-black/20! p-6">
+  <div class="rounded-lg bg-neutral-1 dark:bg-black/20! p-6 my-4">
     <slot />
   </div>
 </template>
--- packages/mixte/src/random/demo/randomNatural.vue
+++ packages/mixte/src/random/demo/randomNatural.vue
@@ -1,11 +1,11 @@
 <template>
   <div flex gap-2 items-center>
     <div grid="~ cols-[auto_1fr] items-center gap-1">
-      min: <el-input-number v-model="min" class="w-30!" controls-position="right" :min="0" />
-      max: <el-input-number v-model="max" class="w-30!" controls-position="right" :min="min" />
+      min: <el-input-number v-model="min" class="w-32!" controls-position="right" :min="0" />
+      max: <el-input-number v-model="max" class="w-32!" controls-position="right" :min="min" />
     </div>
     <button m-btn @click="value = randomNatural(min, max)">点击随机</button>
-    <el-input v-model="value" class="w-30!" readonly />
+    <el-input v-model="value" class="w-36!" readonly />
   </div>
 </template>
 
