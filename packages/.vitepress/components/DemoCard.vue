cd11c20e85a076703755ae861a83cec3165e95bc
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-10 18:25:36 +0800

    [docs] 修改 Demo 卡片组件样式

--- packages/.vitepress/components/DemoCard.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="rounded-lg bg-neutral-1 dark:bg-black/20! p-6 my-4">
-    <slot />
-  </div>
-</template>
---
+++ packages/.vitepress/components/DemoCard/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-6 my-4" b="1 solid neutral-2 dark:neutral-2/36 rounded">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
