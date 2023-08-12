e3260fbe9a9520595054d9be010d0c7d1eead867
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-12 09:32:10 +0800

    [docs] 优化加载 Demo 代码字符串高亮库的方式

--- packages/.vitepress/components/DemoCard/index.vue
+++ packages/.vitepress/components/DemoCard/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="relative b m-theme-border rounded p-6 my-4" :class="{ pt8: showExtra, pb0: hasCode && showCode }">
+  <div class="relative b m-theme-border rounded p-6 my-4" :class="{ pt8: showExtra, pb0: showCode }">
     <slot />
 
     <!-- 功能区 -->
-    <div v-if="showExtra" text="xs lh-none" pos="absolute top-2 right-2">
-      <button m-btn text-base p0 @click="toggleShowCode()">
+    <div v-if="showExtra" class="h-8 mr-2" flex="~ items-center" text="xs lh-none" pos="absolute top-0 right-0">
+      <button m-btn text-lg p0 @click="toggleShowCodeState()">
         <i-material-symbols-code-rounded />
       </button>
     </div>
 
     <!-- 代码区 -->
-    <div v-if="hasCode && showCode" class="text-sm b-t m-theme-border rounded-b overflow-hidden m-(t6 x--6)">
+    <div v-if="showCode" class="text-sm b-t m-theme-border rounded-b overflow-hidden m-(t6 x--6)">
       <div v-if="highlighter" class="[&>.shiki]-(my-0 p-(x6 y3))" v-html="codeHighlight" />
       <div v-else flex="~ justify-center" py-2>
         <i-svg-spinners-ring-resize />
@@ -31,15 +31,20 @@
   });
 
   const hasCode = computed(() => !!code.value);
-  const [showCode, toggleShowCode] = useToggle(true);
+  const [showCodeState, toggleShowCodeState] = useToggle(true);
 
+  const showCode = computed(() => hasCode.value && showCodeState.value);
   const showExtra = computed(() => hasCode.value);
 
-  onMounted(async () => {
-    shiki.setCDN(`https://unpkg.com/shiki@${devDependencies.shiki.replace(/^\^/g, '')}/`);
-    highlighter.value = await shiki.getHighlighter({
-      langs: ['ts'],
-      theme: 'material-theme-darker',
+  onMounted(() => {
+    const unWatch = wheneverImmediate(showCode, async () => {
+      nextTick(() => unWatch());
+
+      shiki.setCDN(`https://unpkg.com/shiki@${devDependencies.shiki.replace(/^\^/g, '')}/`);
+      highlighter.value = await shiki.getHighlighter({
+        langs: ['ts'],
+        theme: 'material-theme-darker',
+      });
     });
   });
 
