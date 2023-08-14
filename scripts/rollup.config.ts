4633c51ec537206a580ac5611fc870cb2668ac45
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-14 11:08:16 +0800

    解决打包后类型定义错误的问题

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -1,15 +1,21 @@
+import { resolve } from 'node:path';
+import type { OutputOptions, RollupOptions } from 'rollup';
+import fs from 'fs-extra';
 import esbuild from 'rollup-plugin-esbuild';
 import dts from 'rollup-plugin-dts';
 import alias from '@rollup/plugin-alias';
-import type { OutputOptions, RollupOptions } from 'rollup';
 import { packages } from '../meta/packages';
+
+fs.removeSync(resolve(__dirname, '../packages/auto-imports.d.ts'));
+fs.removeSync(resolve(__dirname, '../packages/.eslintrc-auto-import.json'));
 
 const configs: RollupOptions[] = [];
 
 const pluginEsbuild = esbuild();
 const pluginAlias = alias({
   entries: {
-    mixte: 'packages/mixte/index.ts',
+    'mixte': 'packages/mixte/index.ts',
+    '@mixte/use': 'packages/use/index.ts',
   },
 });
 const pluginDts = dts({
