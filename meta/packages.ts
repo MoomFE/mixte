e56ab6f5d8738dff705c0185c51c9e40af66983a
倚窗听雨丿醉无心 <994039348@qq.com>
2023-09-15 12:53:19 +0800

    升级依赖类库; 打包相关调整

--- meta/packages.ts
+++ meta/packages.ts
@@ -4,16 +4,16 @@ export const packages = [
     input: 'packages/mixte/index.ts',
     outputFile: 'packages/mixte/dist/index',
   },
-  // @mixte/use
-  {
-    input: 'packages/use/index.ts',
-    outputFile: 'packages/use/dist/index',
-    external: ['mixte'],
-  },
   // @mixte/use/register
   {
     input: 'packages/use/src/register.ts',
     outputFile: 'packages/use/dist/register',
     dtsExternal: ['unplugin-auto-import/types'],
+  },
+  // @mixte/use
+  {
+    input: 'packages/use/index.ts',
+    outputFile: 'packages/use/dist/index',
+    external: ['mixte'],
   },
 ];
