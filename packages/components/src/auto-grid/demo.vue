c5f95c63b7e1cc4e9ee56883c5cbc18e36753608
倚窗听雨丿醉无心 <994039348@qq.com>
2023-09-20 17:42:20 +0800

    [docs] 完善 `AutoGrid` 文档

--- packages/components/src/auto-grid/demo.vue
+++ packages/components/src/auto-grid/demo.vue
@@ -1,22 +1,38 @@
 <template>
-  <AutoGrid item-width="100" gap="10">
-    <template v-for="i in count" :key="i">
-      <div
-        h-8 rounded-sm flex="~ justify-center items-center"
-        text-sm
-        :style="{ backgroundColor: randomColors[i - 1] }"
-      >
-        item-{{ i }}
-      </div>
+  <div grid="~ cols-[auto_auto_1fr] items-center gap-2" text-sm mb5>
+    <span>子元素最小宽度 <small>( px )</small>:</span>
+    <el-input-number v-model="itemWidth" class="w-30! grid-col-span-2" controls-position="right" />
+
+    <template v-if="splitGap">
+      <span>横向间距 <small>( px )</small>:</span>
+      <el-input-number v-model="gapX" class="w-30! grid-col-span-2" controls-position="right" />
+      <span>纵向间距 <small>( px )</small>:</span>
+      <el-input-number v-model="gapY" class="w-30!" controls-position="right" />
     </template>
+    <template v-else>
+      <span>横纵间距 <small>( px )</small>:</span>
+      <el-input-number v-model="gap" class="w-30!" controls-position="right" />
+    </template>
+    <el-checkbox v-model="splitGap" label="拆分" />
+  </div>
+
+  <AutoGrid :item-width="itemWidth" :gap="gap" :gap-x="gapX" :gap-y="gapY">
+    <div
+      v-for="i in count" :key="i"
+      h-8 rounded-sm text-sm flex="~ justify-center items-center"
+      :style="{ backgroundColor: randomColors[i - 1] }"
+    >
+      item-{{ i }}
+    </div>
   </AutoGrid>
 </template>
 
 <script lang="ts" setup>
   import { AutoGrid } from '@mixte/components';
-  import { isPlainObject, random } from 'mixte';
+  import { isNumeric, isPlainObject, random } from 'mixte';
   import { omit } from 'lodash-es';
   import { colors as colorsMap } from '@/.vitepress/shared/unocss.theme';
+  import type { InjectCode, InjectCodeLang } from '@/.vitepress/components/DemoCard/types';
 
   const colors = ([] as string[]).concat(
     ...Object.values(omit(colorsMap, ['dark']))
@@ -26,8 +42,47 @@
       }),
   );
 
-  const count = ref(36);
+  const itemWidth = ref(100);
+
+  const splitGap = ref(false);
+  const gap = ref<number | undefined>(10);
+  const gapX = ref<number | undefined>();
+  const gapY = ref<number | undefined>();
+
+  const count = ref(24);
   const randomColors = computed(() => {
     return Array.from({ length: count.value }, () => colors[random(0, colors.length - 1)]);
   });
+
+  watch(splitGap, (split) => {
+    if (split) {
+      gapX.value = gapY.value = gap.value;
+      gap.value = undefined;
+    }
+    else {
+      gap.value = (isNumeric(gapX.value) || isNumeric(gapY.value)) ? Math.max(gapX.value ?? 0, gapY.value ?? 0) : undefined;
+      gapX.value = gapY.value = undefined;
+    }
+  });
+
+  inject<InjectCodeLang>('codeLang')!.value = 'vue';
+  syncRef(
+    inject<InjectCode>('code')!,
+    computed(() => `
+<template>
+  <MixteAutoGrid item-width="${itemWidth.value}"${
+      isNumeric(gap.value) ? ` gap="${gap.value}"` : ''
+    }${
+      isNumeric(gapX.value) ? ` gap-x="${gapX.value}"` : ''
+    }${
+      isNumeric(gapY.value) ? ` gap-y="${gapY.value}"` : ''
+    }>
+    <div v-for="i in ${count.value}" :key="i">
+      item-{{ i }}
+    </div>
+  </MixteAutoGrid>
+</template>
+    `),
+    { direction: 'rtl' },
+  );
 </script>
