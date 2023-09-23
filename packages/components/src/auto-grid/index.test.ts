0d0c13a981185bcfb45ec5703689ac963e574434
倚窗听雨丿醉无心 <994039348@qq.com>
2023-09-23 12:44:02 +0800

    [components] 完善 `AutoGrid` 单元测试

--- packages/components/src/auto-grid/index.test.ts
+++ packages/components/src/auto-grid/index.test.ts
@@ -40,4 +40,88 @@ describe('AutoGrid', () => {
 
     expect(wrapper.html()).matchSnapshot();
   });
+
+  test('设置横纵间距', () => {
+    const wrapper = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid gap="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const wrapper2 = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid :gap="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const html = wrapper.html();
+    const html2 = wrapper2.html();
+
+    expect(html).toBe(html2);
+    expect(html).matchSnapshot();
+  });
+
+  test('设置横向间距', () => {
+    const wrapper = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid gapX="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const wrapper2 = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid :gapX="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const html = wrapper.html();
+    const html2 = wrapper2.html();
+
+    expect(html).toBe(html2);
+    expect(html).matchSnapshot();
+  });
+
+  test('设置纵向间距', () => {
+    const wrapper = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid gapY="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const wrapper2 = mount({
+      ...emptyOptions,
+      template: `
+        <MixteAutoGrid :gapY="2">
+          <div>item-1</div>
+          <div>item-2</div>
+        </MixteAutoGrid>
+      `,
+    });
+
+    const html = wrapper.html();
+    const html2 = wrapper2.html();
+
+    expect(html).toBe(html2);
+    expect(html).matchSnapshot();
+  });
 });
