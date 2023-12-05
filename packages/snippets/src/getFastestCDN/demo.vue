a81b58c222554ce6e55e7002c37ce49feefb132b
倚窗听雨丿醉无心 <994039348@qq.com>
2023-12-05 18:31:32 +0800

    [docs] 完善 `getFastestCDN` 方法文档 Demo

--- packages/snippets/src/getFastestCDN/demo.vue
+++ packages/snippets/src/getFastestCDN/demo.vue
@@ -1,18 +1,48 @@
 <template>
   <div text-sm grid="~ cols-[auto_1fr] items-center gap-2">
-    pkg: <el-input v-model="pkg" class="w-48!" />
-    version: <el-input v-model="version" class="w-48!" />
-    file: <el-input v-model="file" class="w-48!" />
-    <div grid-col-span-2>
-      <el-button class="c-white!" color="#14b8a6" @click="getFastestCDN(pkg, { version, file })">获取</el-button>
+    pkg: <el-input v-model="pkg" class="w-48!" clearable />
+    version: <el-input v-model="version" class="w-48!" clearable />
+    file: <el-input v-model="file" class="w-66!" clearable />
+    <div grid-col-span-2 flex="~ wrap items-center gap-2">
+      <el-button class="c-white!" color="#14b8a6" :loading="CDN.isLoading" @click="CDN.execute()">获取</el-button>
+      <span c-gray>
+        ← <i text-xs>点击获取当前环境下最快的 CDN 服务</i>
+      </span>
     </div>
+    <div v-if="CDN.isSuccess" class="grid-col-span-2 rounded bg-gray/10 p2">
+      {{ CDN.response }}
+    </div>
+    <el-alert v-else-if="CDN.error" class="grid-col-span-2" type="error" :title="CDN.error" :closable="false" />
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { randomNatural } from 'mixte';
+  import { useRequestReactive } from '@mixte/use';
   import { getFastestCDN } from '@mixte/snippets/getFastestCDN';
 
-  const pkg = ref('mixte');
-  const version = ref<string>();
-  const file = ref<string>();
+  const pkg = ref('');
+  const version = ref('');
+  const file = ref('');
+
+  const examplePkg = [
+    { pkg: 'mixte' },
+    { pkg: 'shiki', version: '0.14.5', file: '/languages/html.tmLanguage.json' },
+    { pkg: 'monaco-editor', version: '0.43.0', file: '/min/vs/basic-languages/yaml/yaml.js' },
+  ];
+
+  const CDN = useRequestReactive(() => {
+    return getFastestCDN(pkg.value, {
+      version: version.value,
+      file: file.value,
+    });
+  });
+
+  onMounted(() => {
+    const example = examplePkg[randomNatural(0, examplePkg.length - 1)];
+
+    pkg.value = example.pkg;
+    version.value = example.version ?? '';
+    file.value = example.file ?? '';
+  });
 </script>
