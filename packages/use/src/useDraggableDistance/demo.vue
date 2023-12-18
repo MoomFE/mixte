ccdb4dbdf474301cdaa17925310402b32f7d335f
倚窗听雨丿醉无心 <994039348@qq.com>
2023-12-18 10:52:33 +0800

    [docs] 完善 `useDraggableDistance` 方法 Demo

--- packages/use/src/useDraggableDistance/demo.vue
+++ packages/use/src/useDraggableDistance/demo.vue
@@ -3,35 +3,42 @@
     <div size-6 pointer-events-none relative>
       <i-ant-design-drag-outlined class="size-6 op-36" />
       <i-ant-design-drag-outlined
-        ref="target"
-        class="size-6 pointer-events-auto absolute"
-        :class="{ 'transition-transform': !isDragging }" :style="{ top: `${y}px`, left: `${x}px` }"
+        ref="targetRef"
+        class="size-6 pointer-events-auto absolute top-0 left-0"
+        :class="{ 'transition-transform': !isDragging }" :style="{ transform: `translate(${x}px, ${y}px)` }"
       />
     </div>
 
     <i-teenyicons-drag-outline class="absolute top-0 right-0 op-66" />
-  </div>
 
-  <div v-if="isDragging" ref="floatingRef" m-popover transition-none :style="floatingStyles">
-    x: {{ x }}, y: {{ y }}
+    <div ref="floatingRef" m-popover transition-none :style="floatingStyles">
+      <template v-if="isDragging">x: {{ x }}, y: {{ y }}</template>
+      <div v-else flex="~ items-center gap-.6">
+        <i-mdi-arrow-top-right-bold-outline class="size-5 relative top-.2 op-66 -ml-.9" />
+        试着拖动一下
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { offset, useFloating } from '@floating-ui/vue';
+  import { autoUpdate, offset, shift, useFloating } from '@floating-ui/vue';
   import { useDraggableDistance } from '@mixte/use';
 
-  const target = ref();
+  const targetRef = ref();
   const floatingRef = ref();
 
-  const { x, y, isDragging } = useDraggableDistance(target);
+  const { x, y, isDragging } = useDraggableDistance(targetRef);
 
-  const { floatingStyles, update: fromFloatingUpdate } = useFloating(target, floatingRef, {
-    placement: 'bottom',
-    middleware: [offset(6)],
+  const { floatingStyles } = useFloating(targetRef, floatingRef, {
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(6),
+      shift({ crossAxis: true }),
+    ],
   });
 
-  watch([x, y], () => {
-    fromFloatingUpdate();
+  watch(isDragging, (isDragging) => {
+    document.body.style.userSelect = isDragging ? 'none' : '';
   });
 </script>
