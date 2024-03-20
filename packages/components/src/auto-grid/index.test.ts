bcba6c0652c38f71330a2c733d15d42fe2c68c61
倚窗听雨丿醉无心 <994039348@qq.com>
2024-03-20 23:11:09 +0800

    [components] 优化 `auto-grid` 组件单元测试

--- packages/components/src/auto-grid/index.test.ts
+++ packages/components/src/auto-grid/index.test.ts
@@ -1,6 +1,8 @@
 import { MixteAutoGrid } from '@mixte/components';
 import { mount } from '@vue/test-utils';
 import { h } from 'vue-demi';
+import postcss from 'postcss';
+import postcssJs from 'postcss-js';
 
 describe('AutoGrid', () => {
   const emptyOptions = {
@@ -12,7 +14,16 @@ describe('AutoGrid', () => {
 
   test('未给组件传入子节点', () => {
     const wrapper = mount(emptyOptions);
-    expect(wrapper.html()).matchSnapshot();
+
+    expect(wrapper.element.children.length).toBe(0);
+
+    expect(postcssJs.objectify(postcss.parse(wrapper.element.getAttribute('style')!))).toStrictEqual({
+      width: '100%',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(0, 1fr)',
+      columnGap: '0px',
+      rowGap: '0px',
+    });
   });
 
   test('给组件传入各个类型的子节点', () => {
@@ -39,7 +50,27 @@ describe('AutoGrid', () => {
       `,
     });
 
-    expect(wrapper.html()).matchSnapshot();
+    const element = wrapper.element;
+    const children = Array.from(element.children);
+
+    // 渲染节点数量
+    expect(children.length).toBe(9);
+
+    // 子节点包裹层及其样式
+    Array.from(children).forEach((child) => {
+      expect((child.cloneNode() as HTMLDivElement).outerHTML).toBe('<div style="overflow: hidden;"></div>');
+    });
+
+    // 子节点内容
+    expect(children[0].innerHTML.trim()).toBe('123');
+    expect(children[1].innerHTML.trim()).toBe('<div>456</div>');
+    expect(children[2].innerHTML.trim()).toBe(mount(emptyOptions).element.outerHTML);
+    expect(children[3].innerHTML.trim()).toBe('<div>item-1</div>');
+    expect(children[4].innerHTML.trim()).toBe('<div>item-2</div>');
+    expect(children[5].innerHTML.trim()).toBe('<div>item-3</div>');
+    expect(children[6].innerHTML.trim()).toBe('<div>item-4</div>');
+    expect(children[7].innerHTML.trim()).toBe('<div>item-5</div>');
+    expect(children[8].innerHTML.trim()).toBe('<div>item-6</div>');
   });
 
   test('使用渲染函数给组件传入各个类型的子节点', () => {
@@ -48,7 +79,7 @@ describe('AutoGrid', () => {
         return h(MixteAutoGrid, {}, {
           default: () => [
             123,
-            '<!-- comment -->',
+            '<!-- fake comment -->',
             h('div', '456'),
             h(MixteAutoGrid),
             Array.from({ length: 6 }).map((_, i) => h('div', `item-${i + 1}`)),
@@ -61,7 +92,31 @@ describe('AutoGrid', () => {
       },
     });
 
-    expect(wrapper.html()).matchSnapshot();
+    const element = wrapper.element;
+    const children = Array.from(element.children);
+
+    // 渲染节点数量
+    expect(children.length).toBe(13);
+
+    // 子节点包裹层及其样式
+    Array.from(children).forEach((child) => {
+      expect((child.cloneNode() as HTMLDivElement).outerHTML).toBe('<div style="overflow: hidden;"></div>');
+    });
+
+    // 子节点内容
+    expect(children[0].innerHTML.trim()).toBe('123');
+    expect(children[1].innerHTML.trim()).toBe('&lt;!-- fake comment --&gt;');
+    expect(children[2].innerHTML.trim()).toBe('<div>456</div>');
+    expect(children[3].innerHTML.trim()).toBe(mount(emptyOptions).element.outerHTML);
+    expect(children[4].innerHTML.trim()).toBe('<div>item-1</div>');
+    expect(children[5].innerHTML.trim()).toBe('<div>item-2</div>');
+    expect(children[6].innerHTML.trim()).toBe('<div>item-3</div>');
+    expect(children[7].innerHTML.trim()).toBe('<div>item-4</div>');
+    expect(children[8].innerHTML.trim()).toBe('<div>item-5</div>');
+    expect(children[9].innerHTML.trim()).toBe('<div>item-6</div>');
+    expect(children[10].innerHTML.trim()).toBe('789');
+    expect(children[11].innerHTML.trim()).toBe('1');
+    expect(children[12].innerHTML.trim()).toBe('2');
   });
 
   test('支持手动传入组件宽度, 会使用传入宽度进行宽度计算每列子元素个数', () => {
@@ -91,7 +146,7 @@ describe('AutoGrid', () => {
     expect(html).matchSnapshot();
   });
 
-  test('设置横纵间距', () => {
+  test('设置横纵间距 ( 样式测试 )', () => {
     const wrapper = mount({
       ...emptyOptions,
       template: `
@@ -144,7 +199,7 @@ describe('AutoGrid', () => {
     expect(html3).matchSnapshot();
   });
 
-  test('设置横向间距', () => {
+  test('设置横向间距 ( 样式测试 )', () => {
     const wrapper = mount({
       ...emptyOptions,
       template: `
@@ -197,7 +252,7 @@ describe('AutoGrid', () => {
     expect(html3).matchSnapshot();
   });
 
-  test('设置纵向间距', () => {
+  test('设置纵向间距 ( 样式测试 )', () => {
     const wrapper = mount({
       ...emptyOptions,
       template: `
