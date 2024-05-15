acb76f368263151cd376070e605fde63b49bcb9e
倚窗听雨丿醉无心 <994039348@qq.com>
2024-05-15 11:42:03 +0800

    [snippets] 在 CI 环境中忽略 `getFastestCDN` 方法单元测试

--- packages/snippets/src/getFastestCDN/index.test.ts
+++ packages/snippets/src/getFastestCDN/index.test.ts
@@ -1,6 +1,7 @@
+import isInCi from 'is-in-ci';
 import { getFastestCDN } from '@mixte/snippets/getFastestCDN';
 
-describe.concurrent('getFastestCDN', () => {
+describe.skipIf(isInCi).concurrent('getFastestCDN', () => {
   it('获取在当前网络环境, 最快的 CDN 服务下的指定类库的根目录地址', { timeout: Number.POSITIVE_INFINITY }, async ({ expect }) => {
     const url = await getFastestCDN('mixte');
 
