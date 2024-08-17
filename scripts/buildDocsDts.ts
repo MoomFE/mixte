ac174489665d490822b8a0d56007b4669bba86eb
倚窗听雨丿醉无心 <994039348@qq.com>
2024-08-17 17:36:26 +0800

    [docs] 解决文档打包报错的问题

--- scripts/buildDocsDts.ts
+++ scripts/buildDocsDts.ts
@@ -2,9 +2,13 @@ import { packages } from '../meta/packages';
 import { pureBuildDts } from './pure-build-dts';
 
 (async () => {
-  // vitepress 中的 twoslash 似乎不能访问 .vue 文件中的类型, 导致文档上的类型无法正确显示
-  // 这里单独构建一份 d.ts 文件
   await pureBuildDts(
-    packages.filter(info => info.vueComponent),
+    packages.filter((info) => {
+      // vitepress 中的 twoslash 似乎不能访问 .vue 文件中的类型, 导致文档上的类型无法正确显示
+      // 这里单独构建一份 d.ts 文件
+      return info.vueComponent
+      // 通过 exports 的方式导出的模块也需要单独构建 d.ts 文件
+        || info.outputFileName === 'toggleThemeViewTransition';
+    }),
   );
 })();
