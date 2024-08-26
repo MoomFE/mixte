1785be03dab98ec58328f0dea5ec6b014ee3c0c2
倚窗听雨丿醉无心 <994039348@qq.com>
2024-08-26 18:03:30 +0800

    [components] 完善组件 `AutoGrid` 组件文档演示

---
+++ packages/components/src/auto-grid/demo/传入各种类型的子元素.preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <MixteAutoGrid item-width="120" gap="8">
+    <!-- 文本节点 -->
+    文本节点
+
+    <!-- 元素节点 -->
+    <div>元素节点</div>
+
+    <!-- 列表渲染会被展开 -->
+    <template v-for="label in '列表渲染会被展开'" :key="label">
+      <div>{{ label }}</div>
+    </template>
+
+    <!-- 组件 -->
+    <el-button w-full type="primary">组件</el-button>
+  </MixteAutoGrid>
+</template>
+
+<script lang="ts" setup>
+  import { MixteAutoGrid } from '@mixte/components/auto-grid';
+</script>
+
+<style lang="scss" scoped>
+  .mixte-auto-grid > :deep(div) {
+    @apply min-h-8 flex-(~ justify-center items-center) text-(sm center) p-2;
+    @apply bg-neutral-200 dark:(bg-neutral-800 text-white);
+  }
+</style>
